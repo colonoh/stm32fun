1a55bd29a139394ea4e3af14a029c8da06263a4e
Steve Warner <steve.j.warner@gmail.com>
2025-08-03 00:31:50 -0700

    New index

--- sai_testing_2/Core/Inc/audio_clip.h
+++ sai_testing_2/Core/Inc/audio_clip.h
@@ -1,22 +1,53 @@
 #pragma once
 
-#include <stdint.h>
+#define AUDIO_CLIP_COUNT 21
 
-const uint8_t audio_clip[] = {
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    // truncated
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-};
-
-#define AUDIO_CLIP_COUNT 2
 typedef struct {
-    uint32_t start;
-    uint32_t length;
+  uint32_t start;
+  uint32_t length;
 } AudioClip;
 
-const AudioClip audio_index[AUDIO_CLIP_COUNT] = {
-    { 0, 107000 }, // Zareyoustillthere.mp3
-    { 107000, 78576 }, // Zgetmad.mp3
+static const AudioClip audio_clips[AUDIO_CLIP_COUNT] = {
+  // Zareyoustillthere
+  { 0x000000, 107034 },
+  // Zcoffeeinmycoffeehole
+  { 0x01A21A, 160530 },
+  // Zgetmad
+  { 0x04152C, 78610 },
+  // Zi_like_your_style
+  { 0x05483E, 287590 },
+  // Zradio-cropped-2
+  { 0x09ABA4, 1412738 },
+  // abcdelicious
+  { 0x1F3A26, 281122 },
+  // abcsingwithme
+  { 0x238448, 1322530 },
+  // bubbles
+  { 0x37B26A, 125986 },
+  // chimesounds
+  { 0x399E8C, 115234 },
+  // creamandsugar
+  { 0x3B60AE, 410146 },
+  // goodjob
+  { 0x41A2D0, 136738 },
+  // happyandyouknowit
+  { 0x43B8F2, 473122 },
+  // hyper
+  { 0x4AF114, 98338 },
+  // iknewyoucouldbrewit
+  { 0x4C7136, 219682 },
+  // onetwothreemoresugarplease
+  { 0x4FCB58, 224290 },
+  // onetwothreesingwithme
+  { 0x53377A, 685090 },
+  // pipessound
+  { 0x5DAB9C, 96802 },
+  // redorangeyellowgreenandblue
+  { 0x5F25BE, 883234 },
+  // sipbleepahhh
+  { 0x6C9FE0, 195106 },
+  // someotherpipesthing
+  { 0x6F9A02, 122914 },
+  // thanksalatte
+  { 0x717A24, 253474 },
 };
